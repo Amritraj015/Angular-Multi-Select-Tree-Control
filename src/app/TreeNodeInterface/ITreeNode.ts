0a134dfec81642b57456abef62c809fa48663943
Amritraj <amritrjakacabu015@gmail.com>
2019-01-22 23:46:23 -0500

    implementing initial DFS Foundations

--- src/app/TreeNodeInterface/ITreeNode.ts
+++ src/app/TreeNodeInterface/ITreeNode.ts
@@ -1,9 +1,34 @@
-//  This is the interface for a tree node
-//  This interface will need to be implemented appropriately wherever the Multi-Select Tree control is being integrated
+//  This is the interface for a tree node.
+//
+//  This interface will need to be implemented appropriately
+//  wherever the Multi-Select Tree control is being integrated.
 
-export interface ITreeNode{
-    nodeName: string;
-    nodeID: number;
-    nodeParent: ITreeNode;
-    nodeChildren: ITreeNode[];
+export interface ITreeNode {
+  //  name of the node
+  nodeName: string;
+
+  //  ID of a node (unique number for each node)
+  nodeID: number;
+
+  //    parent 'p' of a node where: p ∈ {0,1}
+  nodeParent: ITreeNode;
+
+  //    children 'c' of a given node where: c ∈ Z+,
+  nodeChildren: ITreeNode[];
+
+  //    boolean value to represent if a node is selected.
+  //
+  //    true = node is selected
+  //    false = node is not selected
+  nodeSelected: boolean;
+
+  //    'nodeVisisted' is used for the Depth-First Search Algorithm to track if a given
+  //    node has been visited already.
+  //
+  //    The value of this variable is "false" by default and is toggled to "true"
+  //    when the respective node has been rendered on the multi-select control display.
+  //
+  //    true = node visited already
+  //    false = node not vivited yet
+  nodeVisited: false;
 }
--- src/app/ms-tree/ms-tree.component.ts
+++ src/app/ms-tree/ms-tree.component.ts
@@ -1,37 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { ITreeNode } from '../TreeNodeInterface/ITreeNode';
+import { Component, OnInit } from "@angular/core";
+import { ITreeNode } from "../TreeNodeInterface/ITreeNode";
 
 @Component({
-  selector: 'ms-tree',
-  templateUrl: './ms-tree.component.html',
-  styleUrls: ['./ms-tree.component.less']
+  selector: "ms-tree",
+  templateUrl: "./ms-tree.component.html",
+  styleUrls: ["./ms-tree.component.less"]
 })
 export class MSTreeComponent implements OnInit {
-
   leftChild: ITreeNode = {
-    'nodeName': 'leftChild',
-    'nodeID': 1,
-    'nodeParent': null,
-    'nodeChildren': null
+    nodeName: "leftChild",
+    nodeID: 1,
+    nodeParent: null,
+    nodeChildren: [
+      {
+        nodeName: "childLeftChild",
+        nodeID: 3,
+        nodeParent: this.leftChild,
+        nodeChildren: null,
+        nodeSelected: false,
+        nodeVisited: false
+      }
+    ],
+    nodeSelected: false,
+    nodeVisited: false
   };
 
   rightChild: ITreeNode = {
-    'nodeName': 'rightChild',
-    'nodeID': 2,
-    'nodeParent': null,
-    'nodeChildren': null
+    nodeName: "rightChild",
+    nodeID: 2,
+    nodeParent: null,
+    nodeChildren: null,
+    nodeSelected: false,
+    nodeVisited: false
   };
 
   root: ITreeNode = {
-    'nodeName': 'Root',
-    'nodeID': 0,
-    'nodeParent': null,
-    'nodeChildren': [this.leftChild, this.rightChild]
+    nodeName: "Root",
+    nodeID: 0,
+    nodeParent: null,
+    nodeChildren: [this.leftChild, this.rightChild],
+    nodeSelected: false,
+    nodeVisited: false
   };
 
   public tree: ITreeNode[] = [this.root, this.leftChild, this.rightChild];
 
-  ngOnInit() {
+  ngOnInit() {}
+}
+
+//  Custom stack class that is used by the Depth-First Search Algorithm.
+export class DepthFirstSearchStack {
+  stack: ITreeNode[] = [];
+
+  push(value: ITreeNode) {
+    this.stack.push(value);
   }
 
+  pop() {
+    this.stack.pop();
+  }
 }
