0e02ff748285237f39863d4db03a3c1f7884a260
Amritraj <amritraj@talentquest.com>
2019-02-25 14:14:23 -0500

    Final commit of the day - minor comments

--- src/app/components/ms-tree/ms-tree.component.ts
+++ src/app/components/ms-tree/ms-tree.component.ts
@@ -13,41 +13,39 @@ import { GetTreeService } from "src/app/services/get-tree.service";
 export class MSTreeComponent implements OnInit {
   treeControl = new NestedTreeControl<ITreeNode>(node => node.nodeChildren);
   dataSource: MatTreeNestedDataSource<ITreeNode>;
-
   @Output() selectedCount = new EventEmitter<ITreeNode>();
 
+  //  call service to get the tree
   constructor(treeInit: GetTreeService) {
     this.dataSource = treeInit.dataSource;
   }
 
+  //  Automatically expand the first level children when the fly-out loads
   ngOnInit() {
-    //  Automatically expand the first level children when the fly-out loads
     this.treeControl.expand(this.dataSource.data[0]);
   }
 
+  //  check if a node has children
   hasChild = (_: number, node: ITreeNode) =>
     !!node.nodeChildren && node.nodeChildren.length > 0;
 
+  //  check if a node has children
   checkChildren = (node: ITreeNode) => node.nodeChildren.length === 0;
 
+  //  Toggle the checkbox for the current node
+  //  Toggle the checkbox for a node's children
+  //  Expand all the childre4n of a given node when 'selected'
   selectAndExpand(node: ITreeNode): void {
     let stack = new Stack();
 
-    //  Toggle the checkbox for the current node
     node.nodeSelected = !node.nodeSelected;
-
-    //  Toggle the checkbox for a node's children
     stack.pushStack(node);
 
     while (stack.stack.length > 0) {
       let removedNode: ITreeNode = stack.popStack();
-
       if (removedNode.nodeAuthorized)
         removedNode.nodeSelected = node.nodeSelected;
-
-      //  Expand all the childre4n of a given node when 'selected'
       if (removedNode.nodeSelected) this.treeControl.expand(removedNode);
-
       for (let newNode of removedNode.nodeChildren) stack.pushStack(newNode);
     }
 
