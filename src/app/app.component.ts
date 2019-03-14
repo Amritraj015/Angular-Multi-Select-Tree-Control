ace2633485c46698b38d04fe25f87980301b45d2
Amritraj <amritrjakacabu015@gmail.com>
2019-03-13 20:50:54 -0400

    Minor change - Changing selector name for control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "ms-main",
+  selector: "ms-tree-control",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.less"]
 })
